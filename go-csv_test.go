3e7f675ca3c534ee91ae9f2caddff6924abcfd71
coquin <denis.kokin@gmail.com>
2015-07-25 22:20:33 +0400

    Simple strings reader implemented

--- go-csv_test.go
+++ go-csv_test.go
@@ -1,13 +1,11 @@
-package gocsv_test
+package gocsv
 
 import (
-	"github.com/stretchr/testify/assert"
-	// "gocsv"
-	// "strings"
 	"testing"
 )
 
 func TestRead(t *testing.T) {
-	// s := strings.NewReader("1997,Ford,E350,\"ac, abs, moon\",3000.00")
-	assert.Equal(t, true, true, "Yay!")
+	if true != true {
+		t.Error("expected", true, "got", !true)
+	}
 }
--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package gocsv
 
 import (
-	"fmt"
+	"io"
+	"io/ioutil"
 )
 
-func doStuff() {
-	fmt.Println("It works!")
+type Reader struct {
+	b     []byte
+	pos   int
+	Comma rune
+}
+
+func (r *Reader) Read() (record string, err error) {
+	if r.pos >= len(r.b) {
+		return "", io.EOF
+	}
+
+	delim := byte('\n')
+	i := r.pos
+
+	for ; i < len(r.b); i++ {
+		if r.b[i] == delim {
+			break
+		}
+	}
+
+	record = string(r.b[r.pos:i])
+	err = nil
+
+	r.pos = i + 1
+	return
+}
+
+func NewReader(r io.Reader) *Reader {
+	bytes, _ := ioutil.ReadAll(r)
+	return &Reader{bytes, 0, ','}
 }
